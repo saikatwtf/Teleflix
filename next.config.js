8be230cf008c90a008d4ee6e3dcf43e8f6d02030
Saikat <84172414+saikatwtf@users.noreply.github.com>
2025-07-11 17:46:55 +0530

    okkokokkkokok

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   distDir: '.next',
   images: {
+    unoptimized: true,
     domains: ['m.media-amazon.com', 'ia.media-imdb.com', 'placehold.co'],
   },
 }
