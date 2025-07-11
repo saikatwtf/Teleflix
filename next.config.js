9945caad80f556a887c2bfbc556f45ed5da75004
Saikat <84172414+saikatwtf@users.noreply.github.com>
2025-07-11 17:37:22 +0530

    ok

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: '.next',
+  images: {
+    domains: ['m.media-amazon.com', 'ia.media-imdb.com', 'placehold.co'],
+  },
 }
 
 module.exports = nextConfig
